18bea51515cbbcb17dc279a5269b2d4e8204cfb3
ktk8916 <ktk8916@naver.com>
2023-06-09 15:11:49 +0900

    상품 엔티티 추가

---
+++ src/domain/dto/ProductDto.java
@@ -0,0 +1,11 @@
+package domain.dto;
+
+public class ProductDto {
+
+    int id;
+    String name;
+    int makeTime;
+    int categoryId;
+    int price;
+    int qunatity;
+}
---
+++ src/domain/entity/Product.java
@@ -0,0 +1,11 @@
+package domain.entity;
+
+public class Product {
+
+    int id;
+    String name;
+    int makeTime;
+    int categoryId;
+    int price;
+    int qunatity;
+}
