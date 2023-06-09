4a468300c8eb1a55b7a1a16879a695a5912afe1a
ktk8916 <ktk8916@naver.com>
2023-06-09 19:16:19 +0900

    카테고리 기능 추가

--- src/controller/CategoryController.java
+++ src/controller/CategoryController.java
@@ -1,21 +1,33 @@
 package controller;
 
+import domain.entity.Product;
 import service.CategoryService;
+import service.ProductService;
+import utility.LoginUser;
 
 import java.util.Scanner;
 
 public class CategoryController {
 
     private static final CategoryService categoryService = CategoryService.getInstance();
+    private static final ProductService productService = ProductService.getInstance();
     public void selectMode(){
 
+        System.out.println("카테고리 번호를 입력해주세요");
         categoryService.printCategoryList();
 
         Scanner sc = new Scanner(System.in);
 
         while(true){
-            System.out.println("1. 커피 2. 티 3. 스무디");
-            String mode = sc.nextLine();
+
+            int categoryId = Integer.parseInt(sc.nextLine());
+            productService.printProductsByCategoryId(categoryId);
+            System.out.println("상품 번호를 입력해주세요");
+
+            int productId = Integer.parseInt(sc.nextLine());
+            System.out.println("수량을 입력해주세요");
+            int amount = Integer.parseInt(sc.nextLine());
+
         }
     }
 
--- src/repository/ProductRepository.java
+++ src/repository/ProductRepository.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductRepository {
 
@@ -36,5 +38,34 @@ public class ProductRepository {
             throw new RuntimeException(e);
         }
         return product;
+    }
+
+    public List<Product> findByCategoryId(int id){
+
+        Connection conn = new JdbcConnection().getJdbc();
+
+        List<Product> products = new ArrayList<>();
+        String sql = "select * from product " +
+                "where category_id = ?";
+
+        try {
+            PreparedStatement psmt = conn.prepareStatement(sql);
+            psmt.setString(1, String.valueOf(id));
+            ResultSet resultSet = psmt.executeQuery();
+            while (resultSet.next()) {
+                int productId = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int makeTime = resultSet.getInt("");
+                int categoryId = resultSet.getInt("category_id");
+                int price  = resultSet.getInt("price");
+                int quantity = resultSet.getInt("quantity");
+
+                products.add(new Product(productId, name, makeTime, categoryId, price, quantity));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return products;
     }
 }
